5e3953b48851f646f890adb22acc7463002033db
Franco Noroña <franconorona@ootlook.com>
2024-05-26 20:41:21 -0300

    Empezando proyecto

---
+++ script.js
@@ -0,0 +1,36 @@
+const apiUrl = 'https://pokeapi.co/api/v2/pokemon?limit=100'; // Cambia el límite según lo necesites
+const selectElement = document.getElementById("pokemon_select");
+
+function pokemonList()  {
+    return fetch(apiUrl)
+        .then(response => {
+            if(!response.ok) {
+                throw new Error('El linkeado no esta bien:' + response.statusText);
+            }
+            return response.json();
+        })
+        .then(data => {
+            return data.results;
+        })
+        .catch(error => {
+            console.error("Error al realizar fetch de la lista de Pokémon:", error);
+        })
+}
+
+function populateSelect(pokemonList)    {
+    
+    pokemonList.sort((a, b) => a.name.localeCompare(b.name));
+
+    pokemonList.forEach(pokemon => {
+        const optionElement = document.createElement("option");
+        optionElement.value = pokemon.name;
+        optionElement.textContent = pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
+        selectElement.appendChild(optionElement);
+    })
+}
+
+pokemonList().then(pokemonList => {
+    if (pokemonList) {
+        populateSelect(pokemonList);
+    }
+});
